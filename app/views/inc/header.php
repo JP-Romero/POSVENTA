72897dbaaa08fb4d7b8e8b66108a23db6e5381d5
JP-Romero <mcalebr04@gmail.com>
2026-06-24 11:35:46 -0600

    chore: remove Tailwind CSS CDN link from header

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
-    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
     <title><?php echo SITENAME; ?></title>
